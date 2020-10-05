1f5c10803e04fe164d44ce2f1a86c474c4142ba5
Shubham Goyal <shubham1g5@gmail.com>
2020-10-06 00:36:31 +0530

    Notification changes

--- app/src/main/java/org/commcare/dalvik/reminders/notification/RemindersNotificationReceiver.kt
+++ app/src/main/java/org/commcare/dalvik/reminders/notification/RemindersNotificationReceiver.kt
@@ -1,41 +1,24 @@
 package org.commcare.dalvik.reminders.notification
 
-import android.app.PendingIntent
+import android.app.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import org.commcare.dalvik.reminders.MainActivity
-import org.commcare.dalvik.reminders.R
-import org.commcare.dalvik.reminders.ReminderApplication.Companion.DEFAULT_NOTIFICATION_CHANNEL_ID
-import org.commcare.dalvik.reminders.model.Reminder
 
 class RemindersNotificationReceiver : BroadcastReceiver() {
 
     companion object {
-        const val EXTRA_REMINDER = "extra_reminder"
+        const val EXTRA_NOTIFICATION = "extra_notification"
+        const val EXTRA_NOTIFICATION_ID = "extra_notification_id"
+        const val DEFAULT_NOTIFICATION_ID = 1
     }
 
     override fun onReceive(context: Context, intent: Intent?) {
-         intent?.getParcelableExtra<Reminder>(EXTRA_REMINDER)?.let { reminder ->
-
-            val notifyIntent = Intent(context, MainActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            }
-
-            val notification = NotificationCompat.Builder(context, DEFAULT_NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(reminder.title)
-                .setContentText(reminder.detail)
-                .setContentIntent(PendingIntent.getActivity(context, 0, notifyIntent, 0))
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setAutoCancel(true).build()
-
+         intent?.getParcelableExtra<Notification>(EXTRA_NOTIFICATION)?.let { notification ->
             with(NotificationManagerCompat.from(context)) {
-                notify(reminder.id, notification)
+                notify(intent.getIntExtra(EXTRA_NOTIFICATION_ID, DEFAULT_NOTIFICATION_ID), notification)
             }
-
         }
     }
 }
--- app/src/main/java/org/commcare/dalvik/reminders/sync/AlarmScheduler.kt
+++ app/src/main/java/org/commcare/dalvik/reminders/sync/AlarmScheduler.kt
@@ -1,13 +1,20 @@
 package org.commcare.dalvik.reminders.sync
 
 import android.app.AlarmManager
+import android.app.Notification
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationCompat
+import org.commcare.dalvik.reminders.MainActivity
+import org.commcare.dalvik.reminders.R
+import org.commcare.dalvik.reminders.ReminderApplication
 import org.commcare.dalvik.reminders.model.Reminder
 import org.commcare.dalvik.reminders.notification.RemindersNotificationReceiver
-import org.commcare.dalvik.reminders.notification.RemindersNotificationReceiver.Companion.EXTRA_REMINDER
+import org.commcare.dalvik.reminders.notification.RemindersNotificationReceiver.Companion.EXTRA_NOTIFICATION
+import org.commcare.dalvik.reminders.notification.RemindersNotificationReceiver.Companion.EXTRA_NOTIFICATION_ID
 import org.commcare.dalvik.reminders.utils.TimeUtils
+import java.text.ParseException
 
 class AlarmScheduler(private val context: Context) {
 
@@ -24,9 +31,10 @@ class AlarmScheduler(private val context: Context) {
     }
 
     private fun deleteScheduledAlarms(alarmMgr: AlarmManager, oldReminders: List<Reminder>?) {
-        val alarmItent = Intent(context, RemindersNotificationReceiver::class.java)
         oldReminders?.forEach { reminder ->
-            alarmItent.putExtra(EXTRA_REMINDER, reminder)
+            val alarmItent = Intent(context, RemindersNotificationReceiver::class.java)
+            alarmItent.putExtra(EXTRA_NOTIFICATION, buildNotification(reminder))
+            alarmItent.putExtra(EXTRA_NOTIFICATION_ID, reminder.id)
             val alarmPendingIntent: PendingIntent = PendingIntent.getBroadcast(
                 context,
                 REMINDER_NOTIFICATION_REQUEST,
@@ -37,25 +45,45 @@ class AlarmScheduler(private val context: Context) {
         }
     }
 
+    private fun buildNotification(reminder: Reminder): Notification {
+        val notifyIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        return NotificationCompat.Builder(
+            context,
+            ReminderApplication.DEFAULT_NOTIFICATION_CHANNEL_ID
+        )
+            .setSmallIcon(R.mipmap.reminder_launcher)
+            .setContentTitle(reminder.title)
+            .setContentText(reminder.detail)
+            .setContentIntent(PendingIntent.getActivity(context, 0, notifyIntent, 0))
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true).build()
+    }
+
     private fun scheduleAlarms(alarmMgr: AlarmManager, reminders: List<Reminder>) {
-
-        val alarmItent = Intent(context, RemindersNotificationReceiver::class.java)
-
         reminders.forEach { reminder ->
-            val reminderTime = TimeUtils.parseDate(reminder.date)
-            alarmItent.putExtra(EXTRA_REMINDER, reminder)
-            val alarmPendingIntent: PendingIntent = PendingIntent.getBroadcast(
-                context,
-                REMINDER_NOTIFICATION_REQUEST,
-                alarmItent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-            )
+            try {
+                val alarmItent = Intent(context, RemindersNotificationReceiver::class.java)
+                alarmItent.putExtra(EXTRA_NOTIFICATION, buildNotification(reminder))
+                alarmItent.putExtra(EXTRA_NOTIFICATION_ID, reminder.id)
+                val reminderTime = TimeUtils.parseDate(reminder.date)
+                val alarmPendingIntent: PendingIntent = PendingIntent.getBroadcast(
+                    context,
+                    REMINDER_NOTIFICATION_REQUEST,
+                    alarmItent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
 
-            alarmMgr.set(
-                AlarmManager.RTC_WAKEUP,
-                reminderTime.time,
-                alarmPendingIntent
-            )
+                alarmMgr.set(
+                    AlarmManager.RTC_WAKEUP,
+                    reminderTime.time,
+                    alarmPendingIntent
+                )
+            } catch (e: ParseException) {
+                e.printStackTrace()
+            }
         }
     }
 }
