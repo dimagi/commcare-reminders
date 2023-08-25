1bfcceb3e1a581b4ef99b1e968c6078286e632bf
Ahmad Vazirna <avazirna@gmail.com>
2023-08-25 15:25:00 +0200

    Retrieve logged in user ID and pass on to worker

--- app/src/main/java/org/commcare/dalvik/reminders/sync/SyncCasesWorker.kt
+++ app/src/main/java/org/commcare/dalvik/reminders/sync/SyncCasesWorker.kt
@@ -3,14 +3,17 @@ package org.commcare.dalvik.reminders.sync
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import org.commcare.dalvik.reminders.CommCareReceiver.Companion.CC_LOGGED_IN_USER_ID_KEY
 import org.commcare.dalvik.reminders.db.ReminderRepository
 import org.commcare.dalvik.reminders.db.ReminderRoomDatabase
 
-class SyncCasesWorker(appContext: Context, workerParams: WorkerParameters) : CoroutineWorker(appContext, workerParams) {
+class SyncCasesWorker(appContext: Context, private val workerParams: WorkerParameters) : CoroutineWorker(appContext, workerParams) {
 
     override suspend fun doWork(): Result {
+        val data = workerParams.inputData
+        val currentUserId = data.getString(CC_LOGGED_IN_USER_ID_KEY)
         val reminderDao = ReminderRoomDatabase.getDatabase(applicationContext).reminderDao()
-        ReminderRepository(reminderDao).refreshCasesFromCC(applicationContext)
+        ReminderRepository(reminderDao).refreshCasesFromCC(applicationContext, currentUserId)
         return Result.success()
     }
 }
