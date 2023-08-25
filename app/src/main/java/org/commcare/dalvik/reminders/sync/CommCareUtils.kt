a7a3b25eb623a17e255138f5d3fee30b59732c76
Ahmad Vazirna <avazirna@gmail.com>
2023-08-25 15:50:07 +0200

    Skip reminders creation for logged in user

--- app/src/main/java/org/commcare/dalvik/reminders/sync/CommCareUtils.kt
+++ app/src/main/java/org/commcare/dalvik/reminders/sync/CommCareUtils.kt
@@ -1,6 +1,8 @@
 package org.commcare.dalvik.reminders.sync
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import org.commcare.commcaresupportlibrary.CaseUtils
 import org.commcare.dalvik.reminders.model.Reminder
 
@@ -13,8 +15,11 @@ object CommCareUtils {
     private const val DETAIL = "detail"
     private const val TIME = "time"
     private const val SESSION_ENDPOINT = "session_endpoint"
+    // This case property will store a list of User IDs, separated by space or a coma, for whom
+    // the reminder should not be created
+    private const val USERS_TO_SKIP_REMINDER = "users_to_skip_reminder"
 
-    fun getRemindersFromCommCare(context : Context): ArrayList<Reminder> {
+    fun getRemindersFromCommCare(context: Context, currentUserId: String?): ArrayList<Reminder> {
         val remindersCaseCursor = CaseUtils.getCaseMetaData(
             context,
             "case_type = ? and status = 'open'",
@@ -28,6 +33,7 @@ object CommCareUtils {
                 props.add(TIME)
                 props.add(SESSION_ENDPOINT)
                 props.add(CASEID)
+                props.add(USERS_TO_SKIP_REMINDER)
                 val caseProperties = CaseUtils.getCaseProperties(
                     context,
                     remindersCaseCursor.getString(remindersCaseCursor.getColumnIndex(CASE_ID)),
@@ -38,7 +44,12 @@ object CommCareUtils {
                 val time = caseProperties[TIME]
                 val sessionEndpoint = caseProperties[SESSION_ENDPOINT] ?: ""
                 val caseId = caseProperties[CASEID] ?: ""
+                val usersToSkipReminder = caseProperties[USERS_TO_SKIP_REMINDER] ?: ""
 
+                if (currentUserId != null && usersToSkipReminder.isNotEmpty()
+                    && usersToSkipReminder.contains(currentUserId, true)){
+                    continue
+                }
                 if (time != null) {
                     val title = remindersCaseCursor.getString(remindersCaseCursor.getColumnIndex(CASE_NAME))
                     reminders.add(Reminder(0, title, detail, caseId, time, sessionEndpoint))
